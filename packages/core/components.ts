2b89ad8aeb4b5916e430aadae6f4fd19cf9e4e8f
xylanny <lqj04@outlook.com>
2026-09-21 13:23:02 +0800

    feat: 创建核心子包，用于聚合其他功能性的子包，支持按需引入、全量注册；安装对应的依赖。

---
+++ packages/core/components.ts
@@ -0,0 +1,4 @@
+import { UIcon } from "@uglifruits/components";
+import type { Plugin } from "vue";
+
+export default [UIcon] as Plugin[];
---
+++ packages/core/index.ts
@@ -0,0 +1,12 @@
+import "@uglifruits/theme/index.css";
+import components from "./components";
+import { createInstaller } from "../utils";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+const installer = createInstaller(components);
+
+export * from "../components";
+
+export default installer;
