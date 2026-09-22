34c291cc2ddf21e532fdb71c654519ca043f9c29
xylanny <lqj04@outlook.com>
2026-09-22 09:10:17 +0800

    chore: 配置Storybook

--- packages/play/.storybook/main.ts
+++ packages/play/.storybook/main.ts
@@ -1,16 +1,22 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import vue from "@vitejs/plugin-vue";
+import type { StorybookConfig } from "@storybook/vue3-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
-  "framework": "@storybook/vue3-vite"
+  stories: ["../stories/**/*.stories.@(ts|tsx)"], //Story文件位置
+
+  addons: [], // 不使用Storybook的插件
+
+  // 使用vue3+vite，（加载对应的渲染器而将Story渲染为Vue组件、使用Vite作为构建器、挂载Vue特有的docgen）
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {
+      docgen: "vue-component-meta",
+    },
+  },
+
+  // Vite配置（Storybook交给Vite之前，会调用viteFinal做最后一次修改；这里额外挂上@vitejs/plugin-vue，才能编译.vue文件）
+  viteFinal: (config) => mergeConfig(config, { plugins: [vue()] }),
 };
+
 export default config;
--- packages/play/.storybook/preview.ts
+++ packages/play/.storybook/preview.ts
@@ -1,20 +1,32 @@
-import type { Preview } from '@storybook/vue3-vite'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCheck,
+  faHeart,
+  faPlus,
+  faSearch,
+  faSpinner,
+  faStar,
+  faUser,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import type { Preview } from "@storybook/vue3-vite";
+import "@uglifruits/theme/index.css"; // 全局样式导入
 
+library.add(
+  faSearch,
+  faUser,
+  faStar,
+  faHeart,
+  faSpinner,
+  faCheck,
+  faXmark,
+  faPlus,
+);
+
+// 在浏览器环境运行的前置配置
 const preview: Preview = {
   parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+    layout: "centered", // 组件实例在画布正中间，四周留白
   },
 };
 
