ea0a684b9cacdd3b9e11adf90fcf364d97823bf9
xylanny <lqj04@outlook.com>
2026-09-23 20:03:53 +0800

    feat: 注册虚拟列表组件

---
+++ .changeset/tangy-ends-lie.md
@@ -0,0 +1,5 @@
+---
+"@uglifruits/ui": minor
+---
+
+Virtual list component
--- packages/core/components.ts
+++ packages/core/components.ts
@@ -1,4 +1,4 @@
-import { UIcon, UCollapse } from "@uglifruits/components";
+import { UIcon, UCollapse, UVList } from "@uglifruits/components";
 import type { Plugin } from "vue";
 
-export default [UIcon, UCollapse] as Plugin[];
+export default [UIcon, UCollapse, UVList] as Plugin[];
