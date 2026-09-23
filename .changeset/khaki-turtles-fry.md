9c1f79ca4f3273ab672e4302cfb7320deb374d11
xylanny <lqj04@outlook.com>
2026-09-23 21:45:42 +0800

    fix: 修改类型

---
+++ .changeset/khaki-turtles-fry.md
@@ -0,0 +1,5 @@
+---
+"@uglifruits/utils": patch
+---
+
+amend type
--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -4,6 +4,11 @@ import { each } from "lodash-es";
 type SFCWithInstall<T> = T & {
   install: (app: App) => void;
 };
+
+type InstallerPlugin = {
+  install: (app: App) => void;
+};
+
 /**
  * 为单个组件绑上install方法，使其既能作为组件使用，也能作为插件注册
  *
@@ -25,8 +30,10 @@ export function attachInstall<T>(component: T): SFCWithInstall<T> {
  * @param componentPlugs - 已经过withInstall处理、带install方法的组件数组
  * @returns 一个总插件，安装时会依次注册所有传入的组件
  */
-export function createInstaller(componentPlugs: Plugin[]): (app: App) => void {
-  return (app: App) => {
-    each(componentPlugs, (p) => app.use(p));
+export function createInstaller(componentPlugs: Plugin[]): InstallerPlugin {
+  return {
+    install(app: App) {
+      each(componentPlugs, (p) => app.use(p));
+    },
   };
 }
