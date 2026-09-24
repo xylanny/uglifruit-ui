ff54c827dabf86e310413ce9c570694d59cb40aa
xylanny <lqj04@outlook.com>
2026-09-24 10:20:39 +0800

    feat: 注册按钮组件

---
+++ .changeset/sweet-lilies-joke.md
@@ -0,0 +1,5 @@
+---
+"@uglifruits/ui": minor
+---
+
+注册按钮组件
--- packages/core/components.ts
+++ packages/core/components.ts
@@ -1,4 +1,4 @@
-import { UIcon, UCollapse, UList } from "@uglifruits/components";
+import { UIcon, UCollapse, UList, UButton } from "@uglifruits/components";
 import type { Plugin } from "vue";
 
-export default [UIcon, UCollapse, UList] as Plugin[];
+export default [UIcon, UCollapse, UList, UButton] as Plugin[];
